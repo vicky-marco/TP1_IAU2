96674c64062610fdb7484730b53321f33f2c03a7
vicky-marco <vicky_marco@hotmail.com>
2021-08-05 00:23:36 -0300

    1eros pasos

---
+++ TP1.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+
+
+data <- read.csv("https://query.data.world/s/v7xpthpx5kvhyccn2gy2vukmx47qnx", header=TRUE, stringsAsFactors=FALSE)
+
+summary(data)
+
+  
+data2 <- data %>% 
+  select(id, listing_url, last_scraped, summary, description, host_id, host_since, latitude, 
+         longitude, property_type, bathrooms, bedrooms, square_feet, price, monthly_price, 
+         minimum_nights, maximum_nights, availability_30, availability_60, number_of_reviews, 
+         review_scores_rating, review_scores_location, review_scores_value)
+
+head(data2$property_type)
